ea9a87c7e7314d253d5911380c4e4447b1306568
Nikhil Raj <nikhilraj094@gmail.com>
2020-07-07 09:35:54 +0530

    Added a new file index.html and modified existing

--- Addition.java
+++ Addition.java
@@ -1,9 +1,7 @@
 class Addition{
    public static void main(String args[]){
      int a,x=27,y=54;
-      //comment
-
-	//adding some more 
+      
       a=x+y;
       System.out.println(a+ " ");
 }
